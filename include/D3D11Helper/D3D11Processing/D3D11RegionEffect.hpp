009d82a90541909bb3920ca92a40d8a59a9ef2a0
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-10 15:13:41 +0900

    Add region-effect resource-view entry point

--- include/D3D11Helper/D3D11Processing/D3D11RegionEffect.hpp
+++ include/D3D11Helper/D3D11Processing/D3D11RegionEffect.hpp
@@ -31,6 +31,12 @@ public:
         D3D11Resource& dst,
         const RegionEffectDesc& desc);
 
+    void DispatchRegionEffectView(
+        ID3D11DeviceContext* deviceContext,
+        D3D11ResourceView src,
+        D3D11ResourceView dst,
+        const RegionEffectDesc& desc);
+
     D3D11Resource CreateOutputTexture(
         D3D11Core& core,
         UINT width,
